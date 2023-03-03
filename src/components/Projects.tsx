06acc36234a43b5977571826c581b73820a06827
Cesar Camacho <cesarcamacho760@gmail.com>
2023-03-02 17:13:22 -0800

    add links to projects in homepage

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -45,15 +45,31 @@ const Projects = () => {
             </div>
           </div> */}
           <div className="bg-white  w-80 p-4 rounded-lg shadow-sm hover:shadow-xl ease-in duration-150">
-            <div className="relative hover:flex">
+            <div className="relative rounded-lg overflow-hidden mb-6 ">
               <img
-                className="w-72 m-auto h-72 object-cover mb-6 rounded-lg "
+                className="w-72 m-auto h-72 object-cover  "
                 src="assets/pathfinderscreenshot.png"
                 alt="screenshot of pathfinder project"
               />
-              <div className=" hidden  absolute w-full h-full  top-0 justify-center items-center ">
-                <div>Github</div>
-                <div>Link</div>
+              <div className="text-white opacity-0 ease-in duration-100 hover:opacity-90 flex bg-slate-900 absolute w-full h-full top-0 gap-2 flex-col justify-center items-center ">
+                <div className="">
+                  <a
+                    href="https://github.com/Xysar/pathfinder-react"
+                    target="_blank"
+                    className="inline-block hover:bg-yellow-400 hover:text-black rounded-full py-2 px-3 border-2 border-yellow-400 text-yellow-400"
+                  >
+                    Github Repo
+                  </a>
+                </div>
+                <div className="">
+                  <a
+                    href="https://xysar.github.io/pathfinder-react/"
+                    target="_blank"
+                    className="inline-block hover:bg-yellow-400 hover:text-black rounded-full py-2 px-3 border-2 border-yellow-400 text-yellow-400"
+                  >
+                    Open Project
+                  </a>
+                </div>
               </div>
             </div>
             <h1 className="text-center  mb-3 font-bold">Pathfinder</h1>
@@ -72,11 +88,33 @@ const Projects = () => {
             </div>
           </div>
           <div className="bg-white w-80 p-4 rounded-lg shadow-sm hover:shadow-xl ease-in duration-150">
-            <img
-              className="w-72 m-auto h-72 object-cover mb-6 rounded-lg "
-              src="assets/mathbattle.png"
-              alt="Mathbattle Screenshot"
-            />
+            <div className="relative rounded-lg overflow-hidden mb-6 ">
+              <img
+                className="w-72 m-auto h-72 object-cover rounded-lg "
+                src="assets/mathbattle.png"
+                alt="Mathbattle Screenshot"
+              />
+              <div className="text-white opacity-0 ease-in duration-100 hover:opacity-90 flex bg-slate-900 absolute w-full h-full top-0 gap-2 flex-col justify-center items-center ">
+                <div className="">
+                  <a
+                    href="https://github.com/Xysar/MathBattle"
+                    target="_blank"
+                    className="inline-block hover:bg-yellow-400 hover:text-black rounded-full py-2 px-3 border-2 border-yellow-400 text-yellow-400"
+                  >
+                    Github Repo
+                  </a>
+                </div>
+                <div className="">
+                  <a
+                    href="https://xysar.github.io/MathBattle/"
+                    target="_blank"
+                    className="inline-block hover:bg-yellow-400 hover:text-black rounded-full py-2 px-3 border-2 border-yellow-400 text-yellow-400"
+                  >
+                    Open Project
+                  </a>
+                </div>
+              </div>
+            </div>
             <h1 className="text-center mb-3 font-bold">Math Battle</h1>
             <p className="mb-3">
               2D Tower Defense web game designed to teach basic arithmetic.
@@ -89,11 +127,33 @@ const Projects = () => {
             </div>
           </div>
           <div className="bg-white w-80 p-4 rounded-lg shadow-sm hover:shadow-xl ease-in duration-150">
-            <img
-              className="w-72 m-auto h-72 object-cover rounded-lg mb-6"
-              src="public/assets/restaurantScreenshot.png"
-              alt="screenshot of restaurant template website"
-            />
+            <div className="relative rounded-lg overflow-hidden mb-6 ">
+              <img
+                className="w-72 m-auto h-72 object-cover rounded-lg "
+                src="public/assets/restaurantScreenshot.png"
+                alt="screenshot of restaurant template website"
+              />
+              <div className="text-white opacity-0 ease-in duration-100  hover:opacity-90 flex bg-slate-900 absolute w-full h-full top-0 gap-2 flex-col justify-center items-center ">
+                <div className="">
+                  <a
+                    href="https://github.com/Xysar/restaurant-template"
+                    target="_blank"
+                    className="inline-block hover:bg-yellow-400 hover:text-black rounded-full py-2 px-3 border-2 border-yellow-400 text-yellow-400"
+                  >
+                    Github Repo
+                  </a>
+                </div>
+                <div className="">
+                  <a
+                    href="https://restaurant-template-o1465utyi-xysar.vercel.app/"
+                    target="_blank"
+                    className="inline-block hover:bg-yellow-400 hover:text-black rounded-full py-2 px-3 border-2 border-yellow-400 text-yellow-400"
+                  >
+                    Open Project
+                  </a>
+                </div>
+              </div>
+            </div>
             <h1 className="text-center mb-3 font-bold">Restaurant Website</h1>
             <p className="mb-3">
               Custom made website template designed for restaurant.
